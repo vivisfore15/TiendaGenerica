ac08abeddf883e5f38da43f26dcc1c1ba13ea9cb
nur <nurderly@gmail.com>
2021-09-23 19:40:52 -0500

    detalleventa

--- src/main/java/Modelo/VentasDAO.java
+++ src/main/java/Modelo/VentasDAO.java
@@ -2,4 +2,5 @@ package Modelo;
 
 public class VentasDAO {
 
+
 }
--- src/main/java/Modelo/VentasDTO.java
+++ src/main/java/Modelo/VentasDTO.java
@@ -1,6 +1,57 @@
 package Modelo;
 
 public class VentasDTO {
-
+private long codigoventa;
+private long cedula;
+private long Cedula_Usuario;
+private double ivaventa;
+private double totalventa;
+private double valorventa;
+public VentasDTO(long codigoventa, long cedula, long cedula_Usuario, double ivaventa, double totalventa,
+		double valorventa) {
+	super();
+	this.codigoventa = codigoventa;
+	this.cedula = cedula;
+	Cedula_Usuario = cedula_Usuario;
+	this.ivaventa = ivaventa;
+	this.totalventa = totalventa;
+	this.valorventa = valorventa;
+}
+public long getCodigoventa() {
+	return codigoventa;
+}
+public void setCodigoventa(long codigoventa) {
+	this.codigoventa = codigoventa;
+}
+public long getCedula() {
+	return cedula;
+}
+public void setCedula(long cedula) {
+	this.cedula = cedula;
+}
+public long getCedula_Usuario() {
+	return Cedula_Usuario;
+}
+public void setCedula_Usuario(long cedula_Usuario) {
+	Cedula_Usuario = cedula_Usuario;
+}
+public double getIvaventa() {
+	return ivaventa;
+}
+public void setIvaventa(double ivaventa) {
+	this.ivaventa = ivaventa;
+}
+public double getTotalventa() {
+	return totalventa;
+}
+public void setTotalventa(double totalventa) {
+	this.totalventa = totalventa;
+}
+public double getValorventa() {
+	return valorventa;
+}
+public void setValorventa(double valorventa) {
+	this.valorventa = valorventa;
+}
 
 }
